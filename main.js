47f75cac1812c6be66a4efbaffa963a86cc63ce5
Praveen Nagpal <praveennagpal@users.noreply.github.com>
2022-10-16 17:49:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("My First Console Project");
+
+var f_name = "Atiksh";
+console.log(f_name);
+var l_name = "Nagpal";
+console.log(l_name);
+
+var full_name = f_name.concat(l_name);
+console.log(full_name);
+
+var a = 10;
+console.log(a);
+var b = 5;
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var subtract = a - b;
+console.log(subtract);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = a / b;
+console.log(divide);
